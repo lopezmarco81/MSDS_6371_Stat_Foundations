6a240274d50aa01ef035cc30832c8cabd80fc1ae
Marco Lopez <45408152+lopezmarco81@users.noreply.github.com>
2023-09-22 22:40:01 -0500

    Deltas
    I want the deltas if any

--- Unit 2/R Code Permutation v TTest Example.R
+++ Unit 2/R Code Permutation v TTest Example.R
@@ -11,16 +11,16 @@ t.test(skewedDF$Score ~ skewedDF$Treatment)
 
 # number of permutations
 number_of_permutations = 10000;
-#Thi will hold the differnce of sample means generated under the assumption of equality
+#This will hold the difference of sample means generated under the assumption of equality
 xbarholder = c();
 
 # this is the observed difference in sample means that we observed.. is the same the one from t.test above. 
 observed_diff = mean(subset(skewedDF, Treatment == 1)$Score)-mean(subset(skewedDF, Treatment == "2")$Score)
 
-# This counts the number of diffrences of sample means from each permutation that were more extreme than the one observed (diffreence from t.test above)
+# This counts the number of differences of sample means from each permutation that were more extreme than the one observed (difference from t.test above)
 counter = 0;
 
-# this loop run all the permutations and generages and remembers the difference of sample means for each permutation of the labels ... 
+# this loop run all the permutations and generates and remembers the difference of sample means for each permutation of the labels ... 
 # remember, the reason we can permute the labels is because we are assuming the null hypothesis to be true... that the means are 
 # equal under and thus that the treatment has no effect.  That is, that the result we saw for an individual would have been just as 
 #likely to have happened if he or she were in the other group / treatment."
@@ -28,11 +28,11 @@ counter = 0;
 for(i in 1:number_of_permutations)
 {
   scramble = sample(skewedDF$Score,samplesize); #shuffle the observations
-  # the below two lines effectively radnomly applies the extrinsic and intrinsic labels to the observations. 
+  # the below two lines effectively randomly applies the extrinsic and intrinsic labels to the observations. 
   One = scramble[1:(samplesize/2)]; # give the first 23 the extrinsic label
-  Two = scramble[(samplesize/2+1):samplesize]; #give the rest the intrinsic lable. 
-  diff = mean(One)-mean(Two); # find the difference in sample means from current permutation. under the equality assumtion, most of the time this should be close to zero
-  xbarholder[i] = diff; # remember the differene in sample means to compare to the one we observed in the study... 
+  Two = scramble[(samplesize/2+1):samplesize]; #give the rest the intrinsic label. 
+  diff = mean(One)-mean(Two); # find the difference in sample means from current permutation. under the equality assumption, most of the time this should be close to zero
+  xbarholder[i] = diff; # remember the difference in sample means to compare to the one we observed in the study... 
   
   #if the one we observed sticks out then it will be hard to for the diffs to exceed it and that will be evidence that is doesn't 
   #belong and thus is evidence that the difference is not zero and thus that the means are not equal and thus that there is a 
@@ -82,11 +82,11 @@ counter = 0;
 for(i in 1:number_of_permutations)
 {
   scramble = sample(skewedDF$Score,10); #shuffle the observations
-  # the below two lines effectively radnomly applies the extrinsic and intrinsic labels to the observations. 
+  # the below two lines effectively randomly applies the extrinsic and intrinsic labels to the observations. 
   One = scramble[1:5]; # give the first 23 the extrinsic label
-  Two = scramble[6:10]; #give the rest the intrinsic lable. 
-  diff = mean(One)-mean(Two); # find the difference in sample means from current permutation. under the equality assumtion, most of the time this should be close to zero
-  xbarholder[i] = diff; # remember the differene in sample means to compare to the one we observed in the study... 
+  Two = scramble[6:10]; #give the rest the intrinsic label. 
+  diff = mean(One)-mean(Two); # find the difference in sample means from current permutation. under the equality assumption, most of the time this should be close to zero
+  xbarholder[i] = diff; # remember the difference in sample means to compare to the one we observed in the study... 
   
   #if the one we observed sticks out then it will be hard to for the diffs to exceed it and that will be evidence that is doesn't 
   #belong and thus is evidence that the difference is not zero and thus that the means are not equal and thus that there is a 
@@ -100,7 +100,7 @@ for(i in 1:number_of_permutations)
 hist(xbarholder);
 pvalue = counter / number_of_permutations; 
 pvalue
-# the pvalue is the percentage of the differences in sample means that were generaged under 
+# the pvalue is the percentage of the differences in sample means that were generated under 
 #the assumption of equal means exceed the one we observed.  If this percentage is small, then this is evidence that the observed 
 #difference does not belong in this distribution and thus that the difference is not zero and thus that the means are not equal and thus that there is a 
 #treatment effect. 
